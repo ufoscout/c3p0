3dad19507519c8aaa03b37dd75e1dd0e2ae192a9
Francesco Cina <ufoscout@gmail.com>
2019-03-02 14:23:23 +0100

    add md5 spike

--- c3p0_pg_migrate/src/lib.rs
+++ c3p0_pg_migrate/src/lib.rs
@@ -1,2 +1,35 @@
 use postgres::rows::Row;
 use postgres::Connection;
+
+pub struct PgMigrate {
+
+}
+
+struct Migration {
+    up: String,
+    down: String
+}
+
+struct SqlScript {
+    sql: String
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn md5_spike() {
+        use md5::{Md5, Digest};
+        use std::str;
+
+        let some_value = "22341242141241242142";
+        let mut md5 = Md5::default();
+        md5.input(&some_value);
+        let md5_result_hex = md5.result();
+        println!("result is: [{:?}]", &md5_result_hex);
+
+        let md5_result_str = md5_result_hex.iter().map(|&c| format!("{:02x}", c)).collect::<String>();
+        println!("result is: [{}]", &md5_result_str);
+    }
+
+}
