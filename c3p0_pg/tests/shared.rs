888b3ebc831d54efdfad5d3b22daccb5e6556bfc
ufoscout <ufoscout@gmail.com>
2019-03-17 19:35:38 +0100

    test...

--- c3p0_pg/tests/shared.rs
+++ c3p0_pg/tests/shared.rs
@@ -4,6 +4,7 @@ use r2d2_postgres::PostgresConnectionManager;
 use serde_derive::{Deserialize, Serialize};
 use std::sync::Mutex;
 use testcontainers::*;
+use std::sync::atomic::{AtomicUsize, Ordering::SeqCst};
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct TestData {
@@ -17,6 +18,46 @@ lazy_static! {
         Mutex::new(create_postgres_container());
     static ref POOL: Mutex<Pool<PostgresConnectionManager>> = Mutex::new(create_pool());
     pub static ref LOCK: Mutex<()> = Mutex::new(());
+}
+
+struct TestSingleton<T: Drop> {
+    data: Mutex<Option<T>>,
+    init: fn() -> T,
+    callers: Mutex<AtomicUsize>
+}
+
+impl <T: Drop> TestSingleton<T> {
+
+    pub fn new(init: fn() -> T) -> Self {
+        TestSingleton {
+            data: Mutex::new(None),
+            init,
+            callers: Mutex::new(AtomicUsize::new(0))
+        }
+    }
+
+    pub fn get(&self, callback: fn(data: T)) {
+        {
+            let lock= self.callers.lock().unwrap();
+            let callers = lock.load(SeqCst) + 1;
+            lock.store(callers, SeqCst);
+        }
+        {
+            let lock_ = self.data.lock().unwrap();
+
+        }
+        {
+            let lock= self.callers.lock().unwrap();
+            let callers = lock.load(SeqCst) - 1;
+            lock.store(callers, SeqCst);
+
+            if callers == 0 {
+                let mut data = self.data.lock().unwrap();
+                *data = None;
+            }
+        }
+
+    }
 }
 
 pub fn new_connection() -> PooledConnection<PostgresConnectionManager> {
