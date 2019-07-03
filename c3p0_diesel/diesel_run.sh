120535dafc2b4db990954a093f0b8f25ee650739
ufoscout <ufoscout@gmail.com>
2019-07-03 18:33:16 +0200

    Remove diesel. Rename c3p0_json

--- c3p0_diesel/diesel_run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-diesel migration run --database-url=postgres://postgres:postgres@127.0.0.1:5432/postgres
--- c3p0_diesel_macro/tests/migrations/2019-01-30-145559_create_test_data/up.sql
+++
@@ -1,7 +0,0 @@
--- Your SQL goes here
-
-create table TEST_TABLE (
-    ID bigserial primary key,
-    VERSION int not null,
-    DATA JSONB not null
-)
