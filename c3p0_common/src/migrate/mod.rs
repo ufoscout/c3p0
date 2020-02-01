dd0d4319cc9bccbef6069bc5c1ccec1987773544
ufoscout <ufoscout@gmail.com>
2020-02-01 17:29:19 +0100

    Prepare ground for async migrate

--- c3p0_common/src/migrate/mod.rs
+++ c3p0_common/src/migrate/mod.rs
@@ -1,12 +1,19 @@
 use crate::error::C3p0Error;
 use crate::json::codec::DefaultJsonCodec;
 use crate::json::model::{Model, NewModel};
-use crate::json::C3p0Json;
+use crate::json::{C3p0Json};
 use crate::migrate::migration::Migrations;
 use crate::migrate::sql_migration::{to_sql_migrations, SqlMigration};
 use crate::pool::{C3p0Pool, SqlConnection};
 use log::*;
 use serde_derive::{Deserialize, Serialize};
+
+#[cfg(feature = "async")]
+use async_trait::async_trait;
+#[cfg(feature = "async")]
+use crate::pool::{SqlConnectionAsync, C3p0PoolAsync};
+#[cfg(feature = "async")]
+use crate::json::{C3p0JsonAsync};
 
 mod md5;
 pub mod migration;
@@ -152,15 +159,7 @@ impl<CONN: SqlConnection, C3P0: C3p0Pool<CONN = CONN>, MIGRATOR: Migrator<CONN =
         let count = c3p0_json.count_all(conn)?;
 
         if count == 0 {
-            let migration_zero = MigrationData {
-                md5_checksum: "".to_owned(),
-                migration_id: C3P0_INIT_MIGRATION_ID.to_owned(),
-                migration_type: MigrationType::C3P0INIT,
-                execution_time_ms: 0,
-                installed_on_epoch_ms: 0,
-                success: true,
-            };
-            c3p0_json.save(conn, migration_zero.into())?;
+            c3p0_json.save(conn, build_migration_zero().into())?;
         };
 
         Ok(())
@@ -187,33 +186,13 @@ impl<CONN: SqlConnection, C3P0: C3p0Pool<CONN = CONN>, MIGRATOR: Migrator<CONN =
         conn: &mut MIGRATOR::CONN,
     ) -> Result<(), C3p0Error> {
         let migration_history = self.fetch_migrations_history(c3p0_json, conn)?;
-        let migration_history = self.clean_history(migration_history)?;
+        let migration_history = clean_history(migration_history)?;
 
         for i in 0..self.migrations.len() {
             let migration = &self.migrations[i];
 
-            if migration_history.len() > i {
-                let applied_migration = &migration_history[i];
-
-                if applied_migration.data.migration_id.eq(&migration.id) {
-                    if applied_migration.data.md5_checksum.eq(&migration.up.md5) {
-                        continue;
-                    }
-                    return Err(C3p0Error::AlteredMigrationSql {
-                        message: format!(
-                            "Wrong checksum for migration [{}]. Expected [{}], found [{}].",
-                            applied_migration.data.migration_id,
-                            applied_migration.data.md5_checksum,
-                            migration.up.md5
-                        ),
-                    });
-                }
-                return Err(C3p0Error::WrongMigrationSet {
-                    message: format!(
-                        "Wrong migration set! Expected migration [{}], found [{}].",
-                        applied_migration.data.migration_id, migration.id
-                    ),
-                });
+            if check_if_migration_already_applied(&migration_history, &migration, i)? {
+                continue
             }
 
             conn.batch_execute(&migration.up.sql)
@@ -249,33 +228,6 @@ impl<CONN: SqlConnection, C3P0: C3p0Pool<CONN = CONN>, MIGRATOR: Migrator<CONN =
         c3p0_json.fetch_all(conn)
     }
 
-    fn clean_history(
-        &self,
-        migrations: Vec<MigrationModel>,
-    ) -> Result<Vec<MigrationModel>, C3p0Error> {
-        let mut result = vec![];
-
-        for migration in migrations {
-            match migration.data.migration_type {
-                MigrationType::UP => {
-                    result.push(migration);
-                }
-                MigrationType::DOWN => {
-                    let last = result.remove(result.len() - 1);
-                    if !migration.data.migration_id.eq(&last.data.migration_id)
-                        || !last.data.migration_type.eq(&MigrationType::UP)
-                    {
-                        return Err(C3p0Error::CorruptedDbMigrationState {
-                            message: "Migration history is not valid!!".to_owned(),
-                        });
-                    }
-                }
-                MigrationType::C3P0INIT => {}
-            }
-        }
-
-        Ok(result)
-    }
 }
 
 pub trait Migrator: Clone {
@@ -296,4 +248,242 @@ pub trait Migrator: Clone {
         c3p0_json: &Self::C3P0JSON,
         conn: &mut Self::CONN,
     ) -> Result<(), C3p0Error>;
+}
+
+#[cfg(feature = "async")]
+pub trait MigratorAsync: Clone {
+    type CONN: SqlConnectionAsync;
+    type C3P0: C3p0PoolAsync<CONN = Self::CONN>;
+    type C3P0JSON: C3p0JsonAsync<MigrationData, DefaultJsonCodec, CONN = Self::CONN>;
+
+    fn build_cp30_json(&self, table: String, schema: Option<String>) -> Self::C3P0JSON;
+
+    async fn lock_table(
+        &self,
+        c3p0_json: &Self::C3P0JSON,
+        conn: &mut Self::CONN,
+    ) -> Result<(), C3p0Error>;
+
+    async fn lock_first_migration_row(
+        &self,
+        c3p0_json: &Self::C3P0JSON,
+        conn: &mut Self::CONN,
+    ) -> Result<(), C3p0Error>;
+}
+
+#[derive(Clone)]
+#[cfg(feature = "async")]
+pub struct C3p0MigrateAsync<
+    CONN: SqlConnectionAsync,
+    C3P0: C3p0PoolAsync<CONN = CONN>,
+    MIGRATOR: MigratorAsync<CONN = CONN>,
+> {
+    table: String,
+    schema: Option<String>,
+    migrations: Vec<SqlMigration>,
+    c3p0: C3P0,
+    migrator: MIGRATOR,
+}
+
+#[cfg(feature = "async")]
+impl<CONN: SqlConnectionAsync, C3P0: C3p0PoolAsync<CONN = CONN>, MIGRATOR: MigratorAsync<CONN = CONN>>
+C3p0MigrateAsync<CONN, C3P0, MIGRATOR>
+{
+    pub fn new(
+        table: String,
+        schema: Option<String>,
+        migrations: Vec<SqlMigration>,
+        c3p0: C3P0,
+        migrator: MIGRATOR,
+    ) -> Self {
+        Self {
+            table,
+            schema,
+            migrations,
+            c3p0,
+            migrator,
+        }
+    }
+
+    pub async fn migrate(&self) -> Result<(), C3p0Error> {
+        let c3p0_json = self
+            .migrator
+            .build_cp30_json(self.table.clone(), self.schema.clone());
+
+        // Pre Migration
+        self.pre_migration(&c3p0_json)
+            .await
+            .map_err(|err| C3p0Error::MigrationError {
+                message: "C3p0Migrate - Failed to execute pre-migration DB preparation."
+                    .to_string(),
+                cause: Box::new(err),
+            })?;
+
+        // Start Migration
+        self.c3p0
+            .transaction(|conn| {
+                self.migrator.lock_first_migration_row(&c3p0_json, conn)?;
+                Ok(self.start_migration(&c3p0_json, conn)?)
+            })
+            .await
+            .map_err(|err| C3p0Error::MigrationError {
+                message: "C3p0Migrate - Failed to execute DB migration script.".to_string(),
+                cause: err,
+            })
+    }
+
+    pub async fn get_migrations_history(
+        &self,
+        conn: &mut MIGRATOR::CONN,
+    ) -> Result<Vec<MigrationModel>, C3p0Error> {
+        let c3p0_json = self
+            .migrator
+            .build_cp30_json(self.table.clone(), self.schema.clone());
+        c3p0_json.fetch_all(conn)
+    }
+
+    async fn create_migration_zero(
+        &self,
+        c3p0_json: &MIGRATOR::C3P0JSON,
+        conn: &mut MIGRATOR::CONN,
+    ) -> Result<(), C3p0Error> {
+        let count = c3p0_json.count_all(conn).await?;
+
+        if count == 0 {
+            c3p0_json.save(conn, build_migration_zero().into()).await?;
+        };
+
+        Ok(())
+    }
+
+    async fn pre_migration(&self, c3p0_json: &MIGRATOR::C3P0JSON) -> Result<(), C3p0Error> {
+        {
+            let conn = &mut self.c3p0.connection().await?;
+            if let Err(err) = c3p0_json.create_table_if_not_exists(conn).await {
+                warn!("C3p0Migrate - Create table process completed with error. This 'COULD' be fine if another process attempted the same operation concurrently. Err: {}", err);
+            };
+        }
+
+        // Start Migration
+        self.c3p0.transaction(|conn| async {
+            self.migrator.lock_table(&c3p0_json, conn).await?;
+            Ok(self.create_migration_zero(&c3p0_json, conn).await?)
+        })
+    }
+
+    async fn start_migration(
+        &self,
+        c3p0_json: &MIGRATOR::C3P0JSON,
+        conn: &mut MIGRATOR::CONN,
+    ) -> Result<(), C3p0Error> {
+        let migration_history = self.fetch_migrations_history(c3p0_json, conn).await?;
+        let migration_history = clean_history(migration_history)?;
+
+        for i in 0..self.migrations.len() {
+            let migration = &self.migrations[i];
+
+            if check_if_migration_already_applied(&migration_history, &migration, i)? {
+                continue
+            }
+
+            conn.batch_execute(&migration.up.sql).await
+                .map_err(|err| C3p0Error::MigrationError {
+                    message: format!(
+                        "C3p0Migrate - Failed to execute migration with id [{}].",
+                        &migration.id
+                    ),
+                    cause: Box::new(err),
+                })?;
+
+            c3p0_json.save(
+                conn,
+                NewModel::new(MigrationData {
+                    success: true,
+                    md5_checksum: migration.up.md5.clone(),
+                    migration_id: migration.id.clone(),
+                    migration_type: MigrationType::UP,
+                    execution_time_ms: 0,
+                    installed_on_epoch_ms: 0,
+                }),
+            ).await?;
+        }
+
+        Ok(())
+    }
+
+    async fn fetch_migrations_history(
+        &self,
+        c3p0_json: &MIGRATOR::C3P0JSON,
+        conn: &mut MIGRATOR::CONN,
+    ) -> Result<Vec<MigrationModel>, C3p0Error> {
+        c3p0_json.fetch_all(conn).await
+    }
+
+}
+
+
+fn clean_history(
+    migrations: Vec<MigrationModel>,
+) -> Result<Vec<MigrationModel>, C3p0Error> {
+    let mut result = vec![];
+
+    for migration in migrations {
+        match migration.data.migration_type {
+            MigrationType::UP => {
+                result.push(migration);
+            }
+            MigrationType::DOWN => {
+                let last = result.remove(result.len() - 1);
+                if !migration.data.migration_id.eq(&last.data.migration_id)
+                    || !last.data.migration_type.eq(&MigrationType::UP)
+                {
+                    return Err(C3p0Error::CorruptedDbMigrationState {
+                        message: "Migration history is not valid!!".to_owned(),
+                    });
+                }
+            }
+            MigrationType::C3P0INIT => {}
+        }
+    }
+
+    Ok(result)
+}
+
+/// Returns whether the migration was already applied
+fn check_if_migration_already_applied(migration_history: &[MigrationModel], sql_migration: &SqlMigration, check_index: usize) -> Result<bool, C3p0Error> {
+    if migration_history.len() > check_index {
+        let applied_migration = &migration_history[check_index];
+
+        if applied_migration.data.migration_id.eq(&sql_migration.id) {
+            if applied_migration.data.md5_checksum.eq(&sql_migration.up.md5) {
+                return Ok(true);
+            }
+            return Err(C3p0Error::AlteredMigrationSql {
+                message: format!(
+                    "Wrong checksum for migration [{}]. Expected [{}], found [{}].",
+                    applied_migration.data.migration_id,
+                    applied_migration.data.md5_checksum,
+                    sql_migration.up.md5
+                ),
+            });
+        }
+        return Err(C3p0Error::WrongMigrationSet {
+            message: format!(
+                "Wrong migration set! Expected migration [{}], found [{}].",
+                applied_migration.data.migration_id, sql_migration.id
+            ),
+        });
+    };
+    Ok(false)
+}
+
+fn build_migration_zero() -> MigrationData {
+    MigrationData {
+        md5_checksum: "".to_owned(),
+        migration_id: C3P0_INIT_MIGRATION_ID.to_owned(),
+        migration_type: MigrationType::C3P0INIT,
+        execution_time_ms: 0,
+        installed_on_epoch_ms: 0,
+        success: true,
+    }
 }
