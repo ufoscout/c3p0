94aa63b146a40cb1bd9a76ff5304991b9d8c401d
Francesco Cina <ufoscout@gmail.com>
2020-02-01 12:51:08 +0100

    complete pg async pool

--- c3p0_pg_async/src/pool.rs
+++ c3p0_pg_async/src/pool.rs
@@ -86,6 +86,113 @@ impl<'a> SqlConnectionAsync for &'a PgConnection<'a> {
     }
 }
 
+impl <'a> PgConnection<'a> {
+    pub async fn execute(&mut self, sql: &str, params: &[&(dyn ToSql + Sync)]) -> Result<u64, C3p0Error> {
+        match self {
+            PgConnection::Conn(conn) => conn.execute(sql, params).await.map_err(into_c3p0_error),
+            PgConnection::Tx(tx) => tx.execute(sql, params).await.map_err(into_c3p0_error),
+        }
+    }
+
+    pub async fn fetch_one_value<T: FromSqlOwned>(
+        &mut self,
+        sql: &str,
+        params: &[&(dyn ToSql + Sync)],
+    ) -> Result<T, C3p0Error> {
+        self.fetch_one(sql, params, to_value_mapper).await
+    }
+
+    pub async fn fetch_one<T, F: Fn(&Row) -> Result<T, Box<dyn std::error::Error>>>(
+        &mut self,
+        sql: &str,
+        params: &[&(dyn ToSql + Sync)],
+        mapper: F,
+    ) -> Result<T, C3p0Error> {
+        self.fetch_one_optional(sql, params, mapper).await
+            .and_then(|result| result.ok_or_else(|| C3p0Error::ResultNotFoundError))
+    }
+
+    pub async fn fetch_one_optional<T, F: Fn(&Row) -> Result<T, Box<dyn std::error::Error>>>(
+        &mut self,
+        sql: &str,
+        params: &[&(dyn ToSql + Sync)],
+        mapper: F,
+    ) -> Result<Option<T>, C3p0Error> {
+        match self {
+            PgConnection::Conn(conn) => {
+                let stmt = conn.prepare(sql).await.map_err(into_c3p0_error)?;
+                conn.query(&stmt, params)
+                    .await
+                    .map_err(into_c3p0_error)?
+                    .iter()
+                    .next()
+                    .map(|row| mapper(&row))
+                    .transpose()
+                    .map_err(|err| C3p0Error::RowMapperError {
+                        cause: format!("{}", err),
+                    })
+            }
+            PgConnection::Tx(tx) => {
+                let stmt = tx.prepare(sql).await.map_err(into_c3p0_error)?;
+                tx.query(&stmt, params)
+                    .await
+                    .map_err(into_c3p0_error)?
+                    .iter()
+                    .next()
+                    .map(|row| mapper(&row))
+                    .transpose()
+                    .map_err(|err| C3p0Error::RowMapperError {
+                        cause: format!("{}", err),
+                    })
+            }
+        }
+    }
+
+    pub async fn fetch_all<T, F: Fn(&Row) -> Result<T, Box<dyn std::error::Error>>>(
+        &mut self,
+        sql: &str,
+        params: &[&(dyn ToSql + Sync)],
+        mapper: F,
+    ) -> Result<Vec<T>, C3p0Error> {
+        match self {
+            PgConnection::Conn(conn) => {
+                let stmt = conn.prepare(sql).await.map_err(into_c3p0_error)?;
+                conn.query(&stmt, params)
+                    .await
+                    .map_err(into_c3p0_error)?
+                    .iter()
+                    .map(|row| mapper(&row))
+                    .collect::<Result<Vec<T>, Box<dyn std::error::Error>>>()
+                    .map_err(|err| C3p0Error::RowMapperError {
+                        cause: format!("{}", err),
+                    })
+            }
+            PgConnection::Tx(tx) => {
+                let stmt = tx.prepare(sql)
+                    .await
+                    .map_err(into_c3p0_error)?;
+                tx.query(&stmt, params)
+                    .await
+                    .map_err(into_c3p0_error)?
+                    .iter()
+                    .map(|row| mapper(&row))
+                    .collect::<Result<Vec<T>, Box<dyn std::error::Error>>>()
+                    .map_err(|err| C3p0Error::RowMapperError {
+                        cause: format!("{}", err),
+                    })
+            }
+        }
+    }
+
+    pub async fn fetch_all_values<T: FromSqlOwned>(
+        &mut self,
+        sql: &str,
+        params: &[&(dyn ToSql + Sync)],
+    ) -> Result<Vec<T>, C3p0Error> {
+        self.fetch_all(sql, params, to_value_mapper).await
+    }
+}
+
 fn to_value_mapper<T: FromSqlOwned>(row: &Row) -> Result<T, Box<dyn std::error::Error>> {
     Ok(row.try_get(0).map_err(|_| C3p0Error::ResultNotFoundError)?)
 }
