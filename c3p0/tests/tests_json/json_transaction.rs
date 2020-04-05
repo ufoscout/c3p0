c3ac8bcd8765fd983dde6d095be8e691a4b375f3
ufoscout <ufoscout@gmail.com>
2020-04-05 22:25:48 +0200

    Enable all json_transaction tests

--- c3p0/tests/tests_json/json_transaction.rs
+++ c3p0/tests/tests_json/json_transaction.rs
@@ -46,40 +46,45 @@ fn json_should_commit_transaction() {
     })
 }
 
-/*
 #[test]
 fn should_rollback_transaction() {
-    SINGLETON.get(|(pool, _)| {
-        let c3p0: C3p0Impl = pool.clone();
+    test(async {
+
+    let data = data(false).await;
+    let c3p0 = &data.0;
+
         let table_name = format!("TEST_TABLE_{}", rand_string(8));
-        let jpo = C3p0JsonBuilder::new(table_name).build();
+        let jpo = &C3p0JsonBuilder::new(table_name).build();
 
         let model = NewModel::new(TestData {
             first_name: "my_first_name".to_owned(),
             last_name: "my_last_name".to_owned(),
         });
 
-        let result_create_table: Result<(), C3p0Error> = c3p0.transaction(|conn| {
-            assert!(jpo.create_table_if_not_exists(conn).is_ok());
+        let result_create_table: Result<(), C3p0Error> = c3p0.transaction(|mut conn| async move {
+            assert!(jpo.create_table_if_not_exists(&mut conn).await.is_ok());
             Ok(())
-        });
+        }).await;
         assert!(result_create_table.is_ok());
 
-        let result: Result<(), C3p0Error> = c3p0.transaction(|conn| {
-            assert!(jpo.save(conn, model.clone()).is_ok());
-            assert!(jpo.save(conn, model.clone()).is_ok());
-            assert!(jpo.save(conn, model.clone()).is_ok());
+        let result: Result<(), C3p0Error> = c3p0.transaction(|mut conn| async move {
+            assert!(jpo.save(&mut conn, model.clone()).await.is_ok());
+            assert!(jpo.save(&mut conn, model.clone()).await.is_ok());
+            assert!(jpo.save(&mut conn, model.clone()).await.is_ok());
             Err(C3p0Error::ResultNotFoundError)?
-        });
+        }).await;
 
         assert!(result.is_err());
 
         {
-            let conn = &mut c3p0.connection().unwrap();
-            let count = jpo.count_all(conn).unwrap();
-            assert_eq!(0, count);
+            c3p0.transaction::<_,C3p0Error,_, _>(|mut conn| async move {
+                let conn = &mut conn;
+                let count = jpo.count_all(conn).await.unwrap();
+                assert_eq!(0, count);
 
-            assert!(jpo.drop_table_if_exists(conn, true).is_ok());
+                assert!(jpo.drop_table_if_exists(conn, true).await.is_ok());
+                Ok(())
+            }).await.unwrap();
         }
     });
 }
@@ -102,10 +107,14 @@ fn transaction_should_return_internal_error() {
         }
     }
 
-    SINGLETON.get(|(pool, _)| {
-        let c3p0: C3p0Impl = pool.clone();
+    test(async {
 
-        let result: Result<(), _> = c3p0.transaction(|_| Err(CustomError::InnerError));
+    let data = data(false).await;
+    let c3p0 = &data.0;
+
+        let result: Result<(), _> = c3p0.transaction(|_| async {
+            Err(CustomError::InnerError)
+        }).await;
 
         assert!(result.is_err());
 
@@ -115,4 +124,4 @@ fn transaction_should_return_internal_error() {
         }
     });
 }
-*/
+
