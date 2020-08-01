372e5e6e94b60d8293924a7637a0351f7d959162
ufoscout <ufoscout@gmail.com>
2020-08-01 20:25:31 +0200

    drop sqllite

--- c3p0_sqlite/src/blocking/error.rs
+++
@@ -1,9 +0,0 @@
-use c3p0_common::error::C3p0Error;
-
-pub fn into_c3p0_error(error: rusqlite::Error) -> C3p0Error {
-    C3p0Error::DbError {
-        db: "sqlite",
-        cause: format!("{}", error),
-        code: None,
-    }
-}
