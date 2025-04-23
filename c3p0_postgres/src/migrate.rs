14bb9d9bda659daed678399893006d9c11a5a924
Francesco <ufoscout@gmail.com>
2025-04-23 10:26:35 +0200

    fix PgMigration code

--- c3p0_postgres/src/migrate.rs
+++ c3p0_postgres/src/migrate.rs
@@ -1,14 +1,12 @@
 use crate::*;
 use c3p0_common::*;
 
-use async_trait::async_trait;
-
 pub trait PgC3p0MigrateBuilder {
-    fn build(self) -> C3p0Migrate<PgConnection, PgC3p0Pool, PgMigrator>;
+    fn build(self) -> C3p0Migrate<PgMigrator>;
 }
 
 impl PgC3p0MigrateBuilder for C3p0MigrateBuilder<PgC3p0Pool> {
-    fn build(self) -> C3p0Migrate<PgConnection, PgC3p0Pool, PgMigrator> {
+    fn build(self) -> C3p0Migrate<PgMigrator> {
         C3p0Migrate::new(
             self.table,
             self.schema,
@@ -22,26 +20,29 @@ impl PgC3p0MigrateBuilder for C3p0MigrateBuilder<PgC3p0Pool> {
 #[derive(Clone)]
 pub struct PgMigrator {}
 
-#[async_trait]
 impl C3p0Migrator for PgMigrator {
-    type Tx = PgConnection;
+    type Tx<'a> = PgTx<'a>;
     type C3P0 = PgC3p0Pool;
-    type C3P0Json = PgC3p0Json<MigrationData, DefaultJsonCodec>;
+    type C3P0Json = PgC3p0Json<u64, i64, MigrationData, DefaultJsonCodec>;
 
     fn build_cp30_json(
         &self,
         table: String,
         schema: Option<String>,
-    ) -> PgC3p0Json<MigrationData, DefaultJsonCodec> {
-        C3p0JsonBuilder::<PgC3p0Pool>::new(table)
+    ) -> Self::C3P0Json {
+        PgC3p0JsonBuilder::<u64, i64>::new(table)
             .with_schema_name(schema)
             .build()
     }
 
+    async fn batch_execute(&self, sql: &str, conn: &mut Self::Tx<'_>) -> Result<(), C3p0Error> {
+        conn.batch_execute(sql).await
+    }
+
     async fn lock_table(
         &self,
-        c3p0_json: &PgC3p0Json<MigrationData, DefaultJsonCodec>,
-        conn: &mut PgConnection,
+        c3p0_json: &Self::C3P0Json,
+        conn: &mut Self::Tx<'_>,
     ) -> Result<(), C3p0Error> {
         conn.batch_execute(&format!(
             "LOCK TABLE {} IN ACCESS EXCLUSIVE MODE",
@@ -52,8 +53,8 @@ impl C3p0Migrator for PgMigrator {
 
     async fn lock_first_migration_row(
         &self,
-        c3p0_json: &PgC3p0Json<MigrationData, DefaultJsonCodec>,
-        conn: &mut PgConnection,
+        c3p0_json: &Self::C3P0Json,
+        conn: &mut Self::Tx<'_>,
     ) -> Result<(), C3p0Error> {
         let lock_sql = format!(
             r#"select * from {} where {}->>'migration_id' = $1 FOR UPDATE"#,
