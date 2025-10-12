87bf438accf3598f86c5b74879481067d82f911b
Francesco <ufoscout@gmail.com>
2025-10-12 21:50:37 +0200

    Next API stubb

---
+++ next/src/lib.rs
@@ -0,0 +1,62 @@
+use serde::{Deserialize, Serialize};
+
+use crate::record::*;
+
+pub mod record;
+
+pub type UserRecord = Record<User>;
+pub type UserNewRecord = NewRecord<User>;
+
+#[derive(Serialize, Deserialize)]
+pub struct User {
+    pub name: String,
+    pub age: i32,
+}
+
+impl Data for User {
+    const TABLE_NAME: &'static str = "users";
+    type ID = u32;
+    // type CODEC = User;
+    type CODEC = VersionedUser;
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum VersionedUser {
+    V1(String),
+    V2(User),
+}
+
+impl Codec<User> for VersionedUser {
+    fn encode(data: User) -> Self {
+        VersionedUser::V2(data)
+    }
+
+    fn decode(data: Self) -> User {
+        match data {
+            VersionedUser::V1(name) => User { name, age: 0 },
+            VersionedUser::V2(user) => user,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn print_table_name() {
+        let user = UserRecord {
+            id: 1,
+            data: User { name: "John".to_string(), age: 30 }
+        };
+
+        assert_eq!(User::TABLE_NAME, "users");
+        assert_eq!(user.select(), "select * from users where id = $1");
+
+        let user = UserNewRecord {
+            data: User { name: "John".to_string(), age: 30 }
+        };
+
+        assert_eq!(user.insert(), "insert into users");
+    }
+}
---
+++ next/src/record.rs
@@ -0,0 +1,55 @@
+use std::sync::OnceLock;
+
+use serde::{de::DeserializeOwned, Serialize};
+
+pub trait Data: Sized {
+    const TABLE_NAME: &'static str;
+    type ID;
+    type CODEC: Codec<Self>;
+}
+
+pub struct Record<DATA: Data> {
+    pub id: DATA::ID,
+    pub data: DATA
+}
+
+impl <DATA: Data> Record<DATA> {
+    pub fn new(id: DATA::ID, data: DATA) -> Self {
+        Self { id, data }
+    }
+
+    pub fn select(&self) -> &'static str {
+        static QUERY: OnceLock::<String> = OnceLock::new();
+        QUERY.get_or_init(|| format!("select * from {} where id = $1", DATA::TABLE_NAME))
+    }
+
+}
+
+pub struct NewRecord<DATA> {
+    pub data: DATA
+}
+
+impl <DATA: Data> NewRecord<DATA> {
+
+    pub fn insert(&self) -> &'static str {
+        static QUERY: OnceLock::<String> = OnceLock::new();
+        QUERY.get_or_init(|| format!("insert into {}", DATA::TABLE_NAME))
+    }
+}
+
+pub trait Codec<DATA>: Serialize + DeserializeOwned {
+
+    fn encode(data: DATA) -> Self;
+
+    fn decode(data: Self) -> DATA;
+
+}
+
+impl <T: Serialize + DeserializeOwned> Codec<T> for T {
+    
+    #[inline(always)]
+    fn encode(data: T) -> Self { data }
+
+    #[inline(always)]
+    fn decode(data: Self) -> T { data }
+}
